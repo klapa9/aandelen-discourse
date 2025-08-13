289f9f5f20a2aa82e1c6b9fefbcd6eddd0e68221
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-13 15:06:19 +0200

    profiel aandelen tonen

---
+++ assets/javascripts/discourse/initializers/shares-profile.js
@@ -0,0 +1,14 @@
+import { withPluginApi } from "discourse/lib/plugin-api";
+
+export default {
+  name: "shares-profile",
+
+  initialize() {
+    withPluginApi("0.8", (api) => {
+      api.modifyUserCard((userCard) => {
+        const shares = userCard.user.custom_fields.shares || 0;
+        userCard.addBadge(`Aandelen: ${shares}`);
+      });
+    });
+  }
+};
--- plugin.rb
+++ plugin.rb
@@ -7,5 +7,10 @@
 enabled_site_setting :aandelen_enabled
 
 after_initialize do
+  # Voeg een custom user field "shares" toe als het nog niet bestaat
+  unless UserCustomFieldType.exists?(name: "shares")
+    UserCustomFieldType.create!(name: "shares", field_type: "integer")
+  end
+
   Rails.logger.info("Aandelen plugin geladen!")
 end
