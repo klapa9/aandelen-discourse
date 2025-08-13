8d27934c43e0c4d179934e7e30a766a2997dc692
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-13 14:19:06 +0200

    start1

---
+++ plugin.rb
@@ -0,0 +1,11 @@
+# name: Aandelen-plugin
+# about: Beheer en deel van Magast aandelen
+# version: 0.1
+# authors: [Jouw Naam]
+# url: https://github.com/klapa9/magast-aandelen
+
+enabled_site_setting :magast_enabled
+
+after_initialize do
+  # Hier komt alle plugin-logica
+end
