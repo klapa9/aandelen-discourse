f065492dc9a03957d973dcec253ffd48c95c0ded
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-14 16:18:50 +0200

    bugfix routes in plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -6,14 +6,19 @@
 
 enabled_site_setting :aandelen_enabled
 
+# Registreer de JS-routes en templates voor de "shares" tab
+register_asset "javascripts/discourse/routes/user-shares.js", :client_side
+register_asset "javascripts/discourse/templates/user/shares.hbs", :client_side
+
 after_initialize do
   Rails.logger.info("Aandelen plugin geladen!")
 
   User.register_custom_field_type('shares', :integer)
-
+  
   on(:user_created) do |user|
     user.custom_fields['shares'] = 0
     user.save_custom_fields
   end
 end
+
 
