7223d309375e16d5f24f9d91b26cdf1d6a3dab96
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-14 11:28:58 +0200

    refactor: consolidate shares profile initialization and remove deprecated router extension

--- assets/javascripts/discourse/initializers/shares-profile.js
+++ assets/javascripts/discourse/initializers/shares-profile.js
@@ -1,20 +1,29 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 
-function initializeSharesProfile(api) {
-  // De 'user' category zorgt ervoor dat de tab op de juiste plek komt:
-  // de navigatiebalk van het gebruikersprofiel.
-  api.addNavigationBarItem({
-    name: 'shares',
-    displayName: 'Aandelen',
-    route: 'user.shares', // De route waar deze tab naartoe linkt
-    category: 'user'
-  });
-}
-
 export default {
   name: "shares-profile-initializer",
   
   initialize() {
-    withPluginApi("1.0.0", initializeSharesProfile);
+    withPluginApi("1.0.0", (api) => {
+
+      // DEEL 1: Voeg de knop toe aan de navigatiebalk.
+      // Dit deel was al correct.
+      api.addNavigationBarItem({
+        name: 'shares',
+        displayName: 'Aandelen',
+        route: 'user.shares', // We linken naar de 'user.shares' route
+        category: 'user'
+      });
+
+      // DEEL 2: Registreer de 'user.shares' route.
+      // Dit is de nieuwe, correcte manier om de route toe te voegen.
+      api.on('router:map', (router) => {
+        // We zoeken de bestaande 'user' route en voegen er een sub-route aan toe.
+        router.route('user', { path: '/u/:username', resetNamespace: true }, function() {
+          this.route('shares'); // Dit maakt de URL /u/GEBRUIKERSNAAM/shares mogelijk
+        });
+      });
+
+    });
   }
 };
