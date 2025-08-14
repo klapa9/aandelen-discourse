7df1ed064485fa07194e0f9a555d8553a9ef43b8
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-14 14:37:37 +0200

    refactor: simplify shares profile initialization by consolidating navigation and route registration

--- assets/javascripts/discourse/initializers/shares-profile.js
+++ assets/javascripts/discourse/initializers/shares-profile.js
@@ -2,52 +2,20 @@ import { withPluginApi } from "discourse/lib/plugin-api";
 
 export default {
   name: "shares-profile-initializer",
-  
+
   initialize() {
     withPluginApi("1.0.0", (api) => {
-
-      // DEEL 1: Voeg de knop toe aan de navigatiebalk.
-      api.addNavigationBarItem({
-        name: 'shares',
-        displayName: 'Aandelen',
-        route: 'user.shares',
-        category: 'user'
-      });
-
-      // DEEL 2: Registreer de route '/u/:username/shares'
-      api.addRoute('user.shares', { path: 'shares' }, 'user');
-
-      // DEEL 3: Voeg een controller toe voor deze route
-      api.modifyClass('controller:user.shares', {
-        pluginId: 'shares-profile',
-        
-        model(params) {
-          // Hier kun je API calls doen of data ophalen. Voor nu dummy data:
-          return {
-            username: params.username,
-            shares: [
-              { name: 'Apple', amount: 10 },
-              { name: 'Tesla', amount: 5 }
-            ]
-          };
+      // Add a "Aandelen" tab to user profiles
+      api.addProfileTab("shares", {
+        icon: "chart-bar",
+        title: "Aandelen",
+        route: "user.shares",
+        templateName: "user/shares",
+        shouldDisplay(user, currentUser) {
+          // Show tab for all users
+          return !!user;
         }
       });
-
-      // DEEL 4: Voeg een template toe (Ember) voor user.shares
-      api.registerWidget('user.shares', {
-        tagName: 'div',
-        html(attrs, state) {
-          const sharesList = attrs.model.shares.map(
-            s => `<li>${s.name}: ${s.amount} aandelen</li>`
-          ).join('');
-          
-          return `
-            <h2>${attrs.model.username}'s Aandelen</h2>
-            <ul>${sharesList}</ul>
-          `;
-        }
-      });
-
     });
   }
 };
