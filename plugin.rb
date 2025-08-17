6d8f055696ea03fd5ef3a47cd293e405cbd562cf
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-17 08:53:56 +0200

    bf1

--- plugin.rb
+++ plugin.rb
@@ -1,4 +1,4 @@
-# name: aandelen-discourse
+# name: aandelen-plugin
 # about: Beheer aandelen van leden
 # version: 0.1.0
 # authors: klapa9
@@ -9,8 +9,13 @@ enabled_site_setting :aandelen_enabled
 
 after_initialize do
   Rails.logger.info("Aandelen plugin geladen!")
+  module ::Aandelen
+  end
+
   load File.expand_path("../lib/shares/engine.rb", __FILE__)
 end
+
+
 
 
 
