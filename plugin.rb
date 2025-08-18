80270652277d1695cd9546f621edd6c2b621efec
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-18 13:47:09 +0200

    b

--- plugin.rb
+++ plugin.rb
@@ -9,55 +9,49 @@
 enabled_site_setting :shares_enabled
 
 after_initialize do
-  # Load models first
-  require_relative 'app/models/user_share.rb'
-  require_relative 'app/models/share_transaction.rb'
-  
-  # Load serializers
-  require_relative 'app/serializers/user_share_serializer.rb'
-  require_relative 'app/serializers/share_transaction_serializer.rb'
-  
-  # Load controller
-  require_relative 'app/controllers/shares_controller.rb'
+  # Zorg dat basis afhankelijkheden geladen zijn
+  require_dependency "application_controller"
 
-  # Add routes
+  # (Expliciet) laden van je pluginbestanden
+  require_relative "app/models/user_share"
+  require_relative "app/models/share_transaction"
+
+  require_relative "app/serializers/user_share_serializer"
+  require_relative "app/serializers/share_transaction_serializer"
+
+  require_relative "app/controllers/shares_controller"
+
+  # ===== Rails routes =====
   Discourse::Application.routes.append do
-    # bestaande API-routes
-    scope '/shares' do
-      get '/user/:username' => 'shares#show'
-      post '/user/:username/send' => 'shares#send_shares'
+    # Route die HTML serveert zodat Ember kan bootstrappen op /u/:username/shares
+    constraints(username: RouteFormat.username) do
+      get "u/:username/shares" => "shares#index"
+      # optioneel alias
+      get "users/:username/shares" => "shares#index"
     end
 
-    # nieuwe profiel-tab route
-    get '/u/:username/shares' => 'shares#index'
+    # JSON API endpoints voor je controller
+    scope "/shares" do
+      get  "/user/:username"       => "shares#show",         constraints: { username: RouteFormat.username }
+      post "/user/:username/send"  => "shares#send_shares",  constraints: { username: RouteFormat.username }
+    end
   end
 
-  # Add user shares association
-  add_to_class(:user, :user_share_methods) do
+  # ===== User-model uitbreiden =====
+  ::User.class_eval do
     has_one :user_share, dependent: :destroy
-    
+
     def shares_balance
-      user_share&.balance || 0
+      user_share&.balance.to_i
     end
-    
+
     def ensure_user_share
-      self.user_share || create_user_share!(balance: 100)
+      user_share || create_user_share!(balance: 100)
     end
   end
 
-  # Initialize shares for new users
+  # ===== Seed: starttegoed voor nieuwe users =====
   on(:user_created) do |user|
-    user.create_user_share!(balance: 100)
+    user.create_user_share!(balance: 100) unless user.user_share
   end
-
-  # 👇 Nieuw: voeg een tabblad toe aan user-profiel
-  add_to_serializer(:user, :can_see_shares) do
-    true # later kan je hier permissies instellen
-  end
-
-  add_profile_tab("shares",
-    icon: "chart-line",  # FontAwesome icon (je kan bv. 'coins' of 'money-bill' nemen)
-    label: "shares",     # label in de UI
-    path: "shares"       # correspondeert met /u/:username/shares
-  )
 end
