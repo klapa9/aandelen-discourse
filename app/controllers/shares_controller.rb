e868bf578b7ee5bc0b48e511aa0168884f0e66f5
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-21 11:12:15 +0200

    a

--- app/controllers/shares_controller.rb
+++ app/controllers/shares_controller.rb
@@ -2,24 +2,11 @@
 
 class ::SharesController < ::ApplicationController
   def index
-    begin
-      # We proberen de "bulletproof" code uit te voeren die toch crasht.
-      render json: success_json({
-        owner_username: params[:username],
-        shares: [
-          { stock_name: "Test Aandeel", quantity: 123 }
-        ]
-      })
-    rescue StandardError => e
-      # VANG DE CRASH!
-      # In plaats van een generieke 500-error, sturen we nu een JSON terug
-      # met de precieze details van de fout.
-      render json: {
-        error: "Een crash is opgevangen in de SharesController",
-        error_class: e.class.to_s,
-        error_message: e.message,
-        backtrace: e.backtrace.take(15) # De eerste 15 regels van de stack trace
-      }, status: 500
-    end
+    render json: success_json({
+      owner_username: params[:username],
+      shares: [
+        { stock_name: "Test Aandeel", quantity: 123 }
+      ]
+    })
   end
 end
