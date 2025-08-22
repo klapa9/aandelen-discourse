129581a238e44ada9133a6efa81e008809859350
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-22 15:36:59 +0200

    a

--- discourse/components/aandelen-tab.js
+++ discourse/components/aandelen-tab.js
@@ -2,12 +2,9 @@ import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 
 export default class AandelenTab extends Component {
-  // Dummy data blijft hetzelfde
   @tracked aandelenAantal = 500;
 
-  // Hier zit de correctie:
-  // Discourse geeft de gebruiker door als 'model' aan dit component.
   get user() {
-    return this.args.model; // NIET this.args.user
+    return this.args.model;
   }
 }
