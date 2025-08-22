f0d79e75bdbeacdc8ede9001c7ed8996bac5bc47
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-22 14:23:02 +0200

    a

--- plugin.rb
+++ plugin.rb
@@ -8,8 +8,3 @@
 
 # Er is hier GEEN register_asset voor de .hbs-template meer nodig.
 # Discourse laadt dit automatisch.
-
-after_initialize do
-  # Zorg ervoor dat de 'component' naam overeenkomt met het pad en de naam van de template.
-  add_user_card_tab(:aandelen_tab, component: "user/shares", title: "Aandelen", icon: "chart-line")
-end
