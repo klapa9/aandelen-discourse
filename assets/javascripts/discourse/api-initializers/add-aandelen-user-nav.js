86050d49b9b24f6ab12eee8fb082ac8106de850b
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-22 15:26:30 +0200

    a

--- assets/javascripts/discourse/api-initializers/add-aandelen-user-nav.js
+++ assets/javascripts/discourse/api-initializers/add-aandelen-user-nav.js
@@ -1,16 +1,13 @@
 import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("0.1", (api) => {
-  // We gebruiken de JUISTE functie: registerUserMenuTab
-  // Deze is specifiek gemaakt voor tabs op een gebruikersprofiel.
-  api.registerUserMenuTab("aandelen-tab", {
-    // De 'name' wordt gebruikt voor de URL (e.g., /u/username/aandelen)
+  // JUISTE MANIER:
+  // Geef één object mee met daarin alle sleutels,
+  // inclusief de 'component' sleutel.
+  api.registerUserMenuTab({
     name: "aandelen",
-
-    // De tekst die op de tab verschijnt
     label: "Aandelen",
-
-    // Het FontAwesome icoon
     icon: "coins",
+    component: "aandelen-tab", // Deze sleutel moet binnen het object staan
   });
 });
