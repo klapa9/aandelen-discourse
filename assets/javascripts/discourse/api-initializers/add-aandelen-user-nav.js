8a5845b5997171acfa409fbfa62d1faa0f60fd01
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-22 15:35:57 +0200

    a

--- assets/javascripts/discourse/api-initializers/add-aandelen-user-nav.js
+++ assets/javascripts/discourse/api-initializers/add-aandelen-user-nav.js
@@ -1,13 +1,10 @@
 import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("0.1", (api) => {
-  // JUISTE MANIER:
-  // Geef één object mee met daarin alle sleutels,
-  // inclusief de 'component' sleutel.
   api.registerUserMenuTab({
     name: "aandelen",
     label: "Aandelen",
     icon: "coins",
-    component: "aandelen-tab", // Deze sleutel moet binnen het object staan
+    component: "aandelen-tab",
   });
 });
