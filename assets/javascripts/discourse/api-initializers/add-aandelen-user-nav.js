828d2b3e8c632a225dceedec3b0a678c6649c959
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-22 15:48:17 +0200

    a

--- assets/javascripts/discourse/api-initializers/add-aandelen-user-nav.js
+++ assets/javascripts/discourse/api-initializers/add-aandelen-user-nav.js
@@ -1,10 +1,14 @@
 import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("0.1", (api) => {
-  api.registerUserMenuTab({
-    name: "aandelen",
-    label: "Aandelen",
-    icon: "coins",
-    component: "aandelen-tab",
+  // This is the correct, modern way.
+  // We provide a function that returns the object.
+  api.registerUserMenuTab(() => {
+    return {
+      name: "aandelen",
+      label: "Aandelen",
+      icon: "coins",
+      component: "aandelen-tab",
+    };
   });
 });
