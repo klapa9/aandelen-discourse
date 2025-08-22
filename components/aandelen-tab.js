520fa4c0d706da014c0d7d0d8604a92e1a38c57e
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-22 15:12:44 +0200

    a

--- components/aandelen-tab.js
+++ components/aandelen-tab.js
@@ -2,13 +2,12 @@ import Component from "@glimmer/component";
 import { tracked } from "@glimmer/tracking";
 
 export default class AandelenTab extends Component {
-  // We gebruiken @tracked om de data 'reactief' te maken.
-  // Als deze waarde verandert, zal de template automatisch updaten.
-  @tracked aandelenAantal = 500; // Dummy data
+  // Dummy data blijft hetzelfde
+  @tracked aandelenAantal = 500;
 
-  // 'args' bevat alle argumenten die aan het component worden meegegeven.
-  // Voor een user-nav component is 'user' altijd beschikbaar.
+  // Hier zit de correctie:
+  // Discourse geeft de gebruiker door als 'model' aan dit component.
   get user() {
-    return this.args.user;
+    return this.args.model; // NIET this.args.user
   }
 }
