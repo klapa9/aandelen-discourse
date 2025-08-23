092c7b1445d6513f222853b5da731e8621379fc4
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-23 19:55:02 +0200

    a

--- assets/javascripts/discourse/initializers/aandelen-initializer.js
+++ assets/javascripts/discourse/initializers/aandelen-initializer.js
@@ -1,19 +1,18 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
-import { getOwner } from "@ember/application";
 
 export default {
   name: "aandelen-initializer",
   initialize(container) {
-    // Part 1: Define the route using the standard Ember Router
-    const router = getOwner(container).lookup("router:main");
-    router.map(function () {
-      this.route("user", { path: "/u/:username" }, function () {
-        this.route("aandelen", { path: "/aandelen" });
+    // This is the corrected line. We use the 'container' argument directly.
+    const router = container.lookup("router:main");
+    if (router) { // Check if router exists to be safe
+      router.map(function () {
+        this.route("user", { path: "/u/:username" }, function () {
+          this.route("aandelen", { path: "/aandelen" });
+        });
       });
-    });
+    }
 
-    // Part 2: Connect the UI using withPluginApi
-    // This provides the 'api' object that DOES have .connectToPluginOutlet
     withPluginApi("0.1", (api) => {
       api.connectToPluginOutlet(
         "user-nav",
