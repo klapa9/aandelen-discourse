077337c8991a4c2eca340ec7cd950a3d41c810a9
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-23 09:05:33 +0200

    a

--- assets/javascripts/discourse/api-initializers/connect-aandelen-nav-item.js
+++ assets/javascripts/discourse/api-initializers/connect-aandelen-nav-item.js
@@ -1,4 +1,4 @@
-import { apiInitializer } from "discourse/lib/plugin-api";
+import  apiInitializer  from "discourse/lib/plugin-api";
 
 export default apiInitializer("0.1", (api) => {
   // Dit is de moderne, veilige manier om een UI-element toe te voegen.
