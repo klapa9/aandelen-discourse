b3357d85a0bd3d4d1859bf554eb4b9bf1e00403b
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-23 20:11:39 +0200

    a

--- assets/javascripts/discourse/initializers/aandelen-initializer.js
+++ assets/javascripts/discourse/initializers/aandelen-initializer.js
@@ -1,15 +1,12 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
-import { addExtraUserRoute } from "discourse/lib/user-routes";
-import I18n from "I18n";
 
 export default {
   name: "aandelen-initializer",
   initialize() {
-    // This is the official, correct way to add a new user route.
-    addExtraUserRoute("aandelen");
-
     withPluginApi("0.1", (api) => {
-      // This part connects our link component to the UI.
+      // Both functions are called on the 'api' object here.
+      api.addExtraUserRoute("aandelen");
+
       api.connectToPluginOutlet(
         "user-nav",
         "aandelen-user-nav-item"
