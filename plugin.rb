3b91394fdabdd6fecf9b1e5f737b70b88abeb91c
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-23 22:59:27 +0200

    a

--- plugin.rb
+++ plugin.rb
@@ -6,5 +6,3 @@
 # authors: klapa9
 # url: https://github.com/klapa9/aandelen-discourse
 
-register_asset "javascripts/discourse/routes/user-aandelen.js", :client_side
-register_asset "javascripts/discourse/templates/user/aandelen.hbs", :client_side
