81306444ce525ed171356ac38beb140212f9b51f
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-23 22:06:54 +0200

    reset again

--- assets/javascripts/discourse/components/aandelen-tab.js
+++
@@ -1,9 +0,0 @@
-import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
-
-export default class AandelenTab extends Component {
-  @tracked aandelenAantal = 500;
-  get user() {
-    return this.args.model;
-  }
-}
