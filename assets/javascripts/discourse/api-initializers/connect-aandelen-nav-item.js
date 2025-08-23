8d0ae1bc0d864b18bd65a49a7f2a3d916840e2a2
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-23 19:41:45 +0200

    a

--- assets/javascripts/discourse/api-initializers/connect-aandelen-nav-item.js
+++ assets/javascripts/discourse/api-initializers/connect-aandelen-nav-item.js
@@ -1,8 +1,8 @@
-import { apiInitializer } from "discourse/lib/plugin-api"; // The final correction is here
+import { apiInitializer } from "discourse/lib/api";
 
 export default apiInitializer("0.1", (api) => {
   api.connectToPluginOutlet(
-    "user-nav",
+    "user-nav", 
     "aandelen-user-nav-item"
   );
 });
