753d1bb4e8d74d0573296a9da20bf218f8a08102
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-24 09:49:59 +0200

    a

--- assets/javascripts/discourse/initializers/aandelen-modal-initializer.js
+++ assets/javascripts/discourse/initializers/aandelen-modal-initializer.js
@@ -1,28 +1,27 @@
 import { withPluginApi } from "discourse/lib/plugin-api";
 import { ajax } from "discourse/lib/ajax";
 import AandelenModal from "discourse/plugins/aandelen-discourse/discourse/components/aandelen-modal";
+import { service } from "@ember/service"; // BELANGRIJK: importeer 'service'
 
 export default {
   name: "aandelen-modal-initializer",
 
   initialize() {
     withPluginApi("1.0.0", (api) => {
-      // We voegen de actie toe aan de controller van de gebruikerspagina
       api.modifyClass("controller:user", {
-        // Zorg ervoor dat de plugin-id overeenkomt met je folder naam
-        pluginId: "aandelen-discourse", 
-        
+        pluginId: "aandelen-discourse",
+
+        // Stap 1: Injecteer de modal service zodat deze beschikbaar is als 'this.modal'
+        modal: service(),
+
         actions: {
           showAandelenModal() {
-            // Haal de username op van het huidige profiel
             const username = this.model.username;
-            
-            // Haal de modal-service op om pop-ups te kunnen tonen
-            const modal = api.container.lookup("service:modal");
 
-            // Roep onze JSON-endpoint aan
+            // Stap 2: Gebruik de geïnjecteerde service. Dit is veel robuuster.
+            const modal = this.modal;
+
             ajax(`/u/${username}/aandelen.json`).then((data) => {
-              // Toon de modal en geef de opgehaalde data door als 'model'
               modal.show(AandelenModal, { model: data });
             });
           },
