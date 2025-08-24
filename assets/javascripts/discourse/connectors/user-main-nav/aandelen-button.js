b8ed188363ea61cf2e9f96ccda0b90ec0db87d0a
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-24 13:56:29 +0200

    a

--- assets/javascripts/discourse/connectors/user-main-nav/aandelen-button.js
+++ assets/javascripts/discourse/connectors/user-main-nav/aandelen-button.js
@@ -1,11 +1,10 @@
 import Component from "@ember/component";
 import { inject as service } from "@ember/service";
-import MyModal from "discourse/components/my-modal";
 
 export default Component.extend({
   modal: service(),
 
   openModal() {
-    this.modal.show(MyModal);
+    this.modal.show(aandelenModal);
   },
 });
