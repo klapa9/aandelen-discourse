df41f9507eeac2a9f6d6195274355c6c15f24468
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-24 14:34:26 +0200

    a

--- assets/javascripts/discourse/connectors/user-main-nav/aandelen-button.js
+++ assets/javascripts/discourse/connectors/user-main-nav/aandelen-button.js
@@ -1,9 +1,20 @@
-import Component from "@ember/component";
-import { getOwner } from "@ember/application";
+// assets/javascripts/discourse/connectors/user-main-nav/aandelen-button.js
+import { withPluginApi } from "discourse/lib/plugin-api";
+import AandelenModal from "discourse/components/aandelen-modal"; // als je de .js hebt
 
-export default Component.extend({
-  openModal() {
-    const modalService = getOwner(this).lookup("service:modal");
-    modalService.show("aandelen-modal");
-  },
-});
+export default {
+  name: "aandelen-button-init",
+  initialize() {
+    withPluginApi("0.8.7", (api) => {
+      api.decorateConnector("user-main-nav", "aandelen-button", (helper) => ({
+        actions: {
+          openModal() {
+            const modalService = helper.container.lookup("service:modal");
+            // toon de modal
+            modalService.show(AandelenModal);
+          }
+        }
+      }));
+    });
+  }
+};
