58eb503632dccba50226cb92f7ee618be6f33860
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-24 08:36:57 +0200

    a

--- plugin.rb
+++ plugin.rb
@@ -6,8 +6,8 @@
 # authors: klapa9
 # url: https://github.com/klapa9/aandelen-discourse
 
-load File.expand_path('../controllers/aandelen_controller.rb', __FILE__)
-load File.expand_path('../serializers/aandelen_serializer.rb', __FILE__)
+load File.expand_path('./controllers/aandelen_controller.rb', __FILE__)
+load File.expand_path('./serializers/aandelen_serializer.rb', __FILE__)
 
 Discourse::Application.routes.append do
   # Registreert de GET request voor de aandelen pagina en linkt het aan onze controller action
