6f3c37682f133c5f5312f2f40b0d55333d28d683
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-08-25 06:58:40 +0200

    a

--- assets/javascripts/discourse/components/aandelen-button-with-modal.js
+++ assets/javascripts/discourse/components/aandelen-button-with-modal.js
@@ -1,9 +1,17 @@
 import Component from "@glimmer/component";
-import { tracked } from "@glimmer/tracking";
 import { action } from "@ember/object";
+import { tracked } from "@glimmer/tracking";
+import { ajax } from "discourse/lib/ajax";
+import { currentUser } from "discourse/lib/user/current-user";
 
 export default class AandelenButtonWithModal extends Component {
   @tracked modalIsVisible = false;
+  @tracked amount = 1;
+
+  get recipient() {
+    // Dit werkt in user-profile context: de "model" is de user van het profiel
+    return this.args.model;
+  }
 
   @action
   openModal() {
@@ -13,5 +21,27 @@ export default class AandelenButtonWithModal extends Component {
   @action
   closeModal() {
     this.modalIsVisible = false;
+  }
+
+  @action
+  async sendShares() {
+    try {
+      await ajax("/aandelen/send", {
+        type: "POST",
+        data: {
+          from: currentUser.username,
+          to: this.recipient.username,
+          amount: this.amount,
+        },
+      });
+
+      alert(
+        `Succes! Je stuurde ${this.amount} aandelen naar ${this.recipient.username}`
+      );
+      this.closeModal();
+    } catch (e) {
+      console.error("Fout bij versturen aandelen:", e);
+      alert("Er ging iets mis bij het versturen van de aandelen.");
+    }
   }
 }
