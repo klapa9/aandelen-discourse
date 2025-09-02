283dfadba00b74f04cc283da37684d1ddce0b818
pieter vermeulen <pieter.vermeulen@spinewise.be>
2025-09-02 20:53:11 +0000

    invitesfix

--- app/validators/aandelen_invite_validator.rb
+++ app/validators/aandelen_invite_validator.rb
@@ -1,15 +1,22 @@
 # app/validators/aandelen_invite_validator.rb
 class AandelenInviteValidator
   def self.validate(invite)
-    user = invite.inviter
+    # --- CORRECTIE 1 ---
+    # De methode heet .invited_by, niet .inviter
+    user = invite.invited_by
+
+    # Een extra check om zeker te zijn dat de gebruiker bestaat
+    return unless user
 
     if user.created_at.year == Date.today.year
       invite.errors.add(:base, I18n.t("invites.error.new_member"))
       return
     end
 
+    # --- CORRECTIE 2 ---
+    # De databasekolom heet 'invited_by_id', niet 'inviter_id'
     invites_this_year = Invite.where(
-      inviter_id: user.id
+      invited_by_id: user.id
     ).where("created_at >= ?", Date.today.beginning_of_year).count
 
     if invites_this_year >= 1
