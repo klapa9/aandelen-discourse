09cbe2bb8660858737cbb0dcf621670d84f27be6
pieter vermeulen <pieter.vermeulen@spinewise.be>
2026-08-01 20:07:48 +0200

    fixke

--- plugin.rb
+++ plugin.rb
@@ -26,32 +26,12 @@ module Aandelen
 
       Notification.types[:aandelen_received] = AANDELEN_RECEIVED_NOTIFICATION_TYPE
 
-      # Vertel de serializer om de 'data' attribute correct te verwerken.
-      # Discourse kan deze waarde na een update als string, hash of nil leveren.
-      add_to_serializer(:notification, :data) do
-        raw_data = object.data
-
-        return {} if raw_data.blank?
-        return raw_data if raw_data.is_a?(Hash)
-
-        begin
-          JSON.parse(raw_data)
-        rescue JSON::ParserError, TypeError
-          {}
-        end
-      end
-
       Discourse::Application.routes.append do
         get "/aandelen/balance" => "aandelen#balance", defaults: { format: :json }
         get "/aandelen/transactions" => "aandelen#transactions", defaults: { format: :json }
         get "/aandelen/invites" => "aandelen_invites#index", defaults: { format: :json }
         get "/aandelen/users" => "aandelen#users", defaults: { format: :json }
         post "/aandelen/transfer" => "aandelen#transfer", defaults: { format: :json }
-      end
-
-      # automatische transactie bij aanmaken nieuwe gebruiker
-      on(:user_created) do |new_user|
-        Aandelen::AutomatischeTransactie.execute(new_user)
       end
 
       # Validatie voor invites toevoegen
@@ -72,6 +52,29 @@ after_initialize do
     require_dependency "#{Rails.root}/plugins/aandelen-discourse/app/jobs/regular/send_aandelen_messages.rb"
     load File.expand_path("../app/controllers/aandelen_controller.rb", __FILE__)
     load File.expand_path("../app/controllers/aandelen_invites_controller.rb", __FILE__)
+
+    # Vertel de serializer om de 'data' attribute correct te verwerken.
+    # Discourse kan deze waarde na een update als string, hash of nil leveren.
+    add_to_serializer(:notification, :data) do
+      raw_data = object.data
+
+      if raw_data.blank?
+        {}
+      elsif raw_data.is_a?(Hash)
+        raw_data
+      else
+        begin
+          JSON.parse(raw_data)
+        rescue JSON::ParserError, TypeError
+          {}
+        end
+      end
+    end
+
+    # automatische transactie bij aanmaken nieuwe gebruiker
+    on(:user_created) do |new_user|
+      Aandelen::AutomatischeTransactie.execute(new_user)
+    end
 
     Aandelen::PluginHooks.register!
   rescue => e
